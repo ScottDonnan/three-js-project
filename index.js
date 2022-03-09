23cad0b34712413cc4e611b9d7d743c9f74af0e1
Scott Donnan <scott.donnan@me.com>
2022-03-08 21:29:52 -0600

    playing around with threejs

--- index.js
+++ index.js
@@ -1,17 +1,73 @@
-// Our Javascript will go here.
+// camera
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
+//renderer
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
-const geometry = new THREE.TorusKnotGeometry(10, 3, 64, 8, 2, 3);
-const material = new THREE.MeshBasicMaterial( { color: 0xffff00 } );
-const cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
+//object
+const geometry = new THREE.BoxGeometry(1,1,1);
 
-camera.position.z = 50;
+function makeShape(geometry, color, x = 0, y = 0) {
+    const material = new THREE.MeshPhongMaterial( {color} )
+
+    const cube = new THREE.Mesh( geometry, material )
+
+    scene.add( cube )
+
+    cube.position.x = x
+    cube.position.y = y
+
+    return cube;
+}
+
+const cubes = [
+    makeShape(geometry, 0x44aa88, 0),
+    makeShape(geometry, 0x8844aa, -2),
+    makeShape(geometry, 0xaa8844, 2),
+    makeShape(geometry, 0xaa8844, 0, 2),
+]
+
+//lighting
+const directionalLight = new THREE.DirectionalLight( 0xffffff, 1)
+directionalLight.position.set(-1, 2, 6)
+directionalLight.castShadow = true;
+scene.add( directionalLight )
+
+camera.position.z = 5;
+
+//scene adjustments
+const color = new THREE.Color();
+scene.background = color;
+
+
+//sound
+const AudioContext = window.AudioContext || window.webkitAudioContext;
+const audioContext = new AudioContext();
+const audioElement = document.querySelector('audio')
+const track = audioContext.createMediaElementSource(audioElement)
+track.connect(audioContext.destination)
+
+document.querySelector('#play').addEventListener('click', function() {
+    debugger;
+    if(audioContext.state === 'suspended') {
+        audioContext.resume()
+
+    } else {
+        audio
+    }
+
+    if(this.dataset.playing === true) {
+        audioElement.pause()
+    } else {
+        audioElement.play()
+
+    }
+})
+
+
 
 
 // let myVar=1;
@@ -42,9 +98,14 @@ function render() {
 }
 
 function animate() {
+    console.log("animate clicked")
     myReq = requestAnimationFrame( animate )
-    cube.rotation.x += .01;
-    cube.rotation.y += .01;
+
+    cubes.forEach(cube => {
+        cube.rotation.x += .01;
+        cube.rotation.y += .01;
+    })
+    
     renderer.render( scene, camera );
 }
 
